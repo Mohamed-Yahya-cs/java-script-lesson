42ddae694283ef3aae354902ca1bed54de0fb490
Mohamed-Yahya-cs <m.yahya@share.epsb.ca>
2018-09-25 13:51:53 -0600

    nice

--- Function Array Assignment/index.js
+++ Function Array Assignment/index.js
@@ -1,18 +1,25 @@
 console.log("Using array-data-strucutre, write a function(s) to switch the content of both arrays in the second element ... the last name.")
 
 // Your Function Goes here
+function rerwiteLastNameYahya (lastName1, lastName2) {
+   MohamedYahya[1] = lastName2
+}
+function rerwiteLastNameSpence (lastName1, lastName2) {
+  return MeganSpence[1] = lastName2
 
-
-let MarkMercer = [];
+let MohamedYahya = [];
 let MeganSpence = [];
 
 // firstName, lastName, age, ageAfraid, hair, male, female
-MarkMercer = ["Mark", "Mercer", 13, 29, null, true, false];
+MohamedYahya = ["Mohamed", "Yahya", 13, 29, null, true, false];
 MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
 
 //Your code goes here
+rerwiteLastNameYahya (MohamedYahya[1], MeganSpence[1]); {
+rerwiteLastNameSpence (MeganSpence[1], MohamedYahya[1]);  
 
+}
 
 // Print first names only
-console.log("Last Name of MarkMercer is: " + MarkMercer[1]); //Should return Spence
+console.log("Last Name of MohamedYahya is: " + MohamedYahya[1]); //Should return Spence
 console.log("Last Name of MeganSpence is: " + MeganSpence[1]); //Should return Mercer
