a438d7acef74afe81bf999e7a868d679deca0d8f
Mohamed-Yahya-cs <m.yahya@share.epsb.ca>
2018-10-04 13:22:06 -0600

    last one

--- Greeting/index.js
+++ Greeting/index.js
@@ -1,4 +1,4 @@
-let hour = 87556465356468; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+let hour = 12; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
 if (hour = 06, 07, 08, 09, 10, 11) console.log('Good Morning');
 else if (hour = 12, 13, 14, 15, 16, 17) console.log('Good Afternoon');
 else console.log('Good Evning');
